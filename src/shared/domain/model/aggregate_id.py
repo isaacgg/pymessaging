47daa858ddf068e08aa6bd3c85d4337f99f2063e
isaacgg <isaac.gonzalezg91@gmail.com>
2023-05-18 16:24:46 +0200

    Deleted unrelated code

--- src/shared/domain/model/aggregate_id.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from typing import Union
-from uuid import UUID
-
-
-@dataclass(frozen=True)
-class AggregateId:
-    id: Union[UUID, str]
--- src/shared/domain/model/strings.py
+++
@@ -1,5 +0,0 @@
-import re
-
-
-def to_snake_case(text: str) -> str:
-    return re.sub(r'(?<!^)(?=[A-Z])', '_', text).lower()
